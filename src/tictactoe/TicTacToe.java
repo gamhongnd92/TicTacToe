dbe67c34b0c0cebd8b1d433919a41e23c58fe7d8
Gam <trant48@student.douglascollege.ca>
2021-02-02 10:51:33 -0800

    Create form

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,225 @@
+package tictactoe;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class TicTacToe {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TicTacToe window = new TicTacToe();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TicTacToe() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 643, 429);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(3, 5, 2, 2));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_1);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+	
+		
+		
+		JButton btn1 = new JButton("");
+		btn1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_1.add(btn1, BorderLayout.CENTER);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn2 = new JButton("");
+		btn2.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_2.add(btn2, BorderLayout.CENTER);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn3 = new JButton("");
+		btn3.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_3.add(btn3, BorderLayout.CENTER);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_4);
+		panel_4.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Player X:\r\n");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_4.add(lblNewLabel, BorderLayout.CENTER);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_5);
+		panel_5.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblCountX = new JLabel("0");
+		lblCountX.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCountX.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_5.add(lblCountX, BorderLayout.CENTER);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_6);
+		panel_6.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn4 = new JButton("");
+		panel_6.add(btn4, BorderLayout.CENTER);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_7);
+		panel_7.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn5 = new JButton("");
+		btn5.setFont(new Font("Dialog", Font.BOLD, 20));
+		panel_7.add(btn5, BorderLayout.CENTER);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_8);
+		panel_8.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn6 = new JButton("");
+		btn6.setFont(new Font("Dialog", Font.BOLD, 20));
+		panel_8.add(btn6, BorderLayout.CENTER);
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_9);
+		panel_9.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("Player O:\r\n");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_9.add(lblNewLabel_1, BorderLayout.CENTER);
+		
+		JPanel panel_10 = new JPanel();
+		panel_10.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_10);
+		panel_10.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblCountO = new JLabel("0");
+		lblCountO.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCountO.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_10.add(lblCountO, BorderLayout.CENTER);
+		
+		JPanel panel_11 = new JPanel();
+		panel_11.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_11);
+		panel_11.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn7 = new JButton("");
+		btn7.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_11.add(btn7, BorderLayout.CENTER);
+		
+		JPanel panel_12 = new JPanel();
+		panel_12.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_12);
+		panel_12.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn8 = new JButton("");
+		btn8.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_12.add(btn8, BorderLayout.CENTER);
+		
+		JPanel panel_13 = new JPanel();
+		panel_13.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_13);
+		panel_13.setLayout(new BorderLayout(0, 0));
+		
+		JButton btn9 = new JButton("");
+		btn9.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_13.add(btn9, BorderLayout.CENTER);
+		
+		JPanel panel_14 = new JPanel();
+		panel_14.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_14);
+		panel_14.setLayout(new BorderLayout(0, 0));
+		
+		
+		// btnReset
+		JButton btnReset = new JButton("Reset");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn1.setText(null);
+				btn2.setText(null);
+				btn3.setText(null);
+				btn4.setText(null);
+				btn5.setText(null);
+				btn6.setText(null);
+				btn7.setText(null);
+				btn8.setText(null);
+				btn9.setText(null);
+				
+				btn1.setBackground(Color.LIGHT_GRAY);
+				btn2.setBackground(Color.LIGHT_GRAY);
+				btn3.setBackground(Color.LIGHT_GRAY);
+				btn4.setBackground(Color.LIGHT_GRAY);
+				btn5.setBackground(Color.LIGHT_GRAY);
+				btn6.setBackground(Color.LIGHT_GRAY);
+				btn7.setBackground(Color.LIGHT_GRAY);
+				btn8.setBackground(Color.LIGHT_GRAY);
+				btn9.setBackground(Color.LIGHT_GRAY);
+				
+				
+			}
+		});
+		btnReset.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_14.add(btnReset, BorderLayout.CENTER);
+		
+		JPanel panel_15 = new JPanel();
+		panel_15.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel.add(panel_15);
+		panel_15.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel_15.add(btnExit, BorderLayout.CENTER);
+	}
+
+}
